7aa4e1cab267f340a60889f2c21f80dbcefc320a
York <2890353332@qq.com>
2019-03-24 00:12:42 +0800

    over

--- app/main/views.py
+++ app/main/views.py
@@ -37,7 +37,7 @@ def index_views():
         for j in idos.all():
             l2.append(j[0])
     # 每页显示的记录数量
-    pageSize = 20
+    pageSize = 15
     # 接收前段传过来的参数page,,如果没有传递过来，则默认为１，并保存在page变量中
     page = int(request.args.get('page', 1))
     ost = (page - 1) * pageSize
@@ -82,7 +82,7 @@ def list_views():
         for j in idos.all():
             l2.append(j[0])
     # 每页显示的记录数量
-    pageSize = 20
+    pageSize = 15
     # 接收前段传过来的参数page,,如果没有传递过来，则默认为１，并保存在page变量中
     page = int(request.args.get('page', 1))
     ost = (page - 1) * pageSize
@@ -150,7 +150,7 @@ def search_views():
     notes = notes[::-1]
 
     # 每页显示的记录数量
-    pageSize = 20
+    pageSize = 15
     # 接收前段传过来的参数page,,如果没有传递过来，则默认为１，并保存在page变量中
     page = int(request.args.get('page', 1))
     ost = (page - 1) * pageSize
@@ -181,13 +181,6 @@ def search_views():
 @main.route('/friends')
 def friends_views():
     if "id" in session and "name" in session:
-        id = session['id']
-        user = Users.query.filter_by(id=id).first()
-        fans = db.session.query(User_attention.user_fan_id).filter(User_attention.user_star_id == id)
-        idos = db.session.query(User_attention.user_star_id).filter(User_attention.user_fan_id == id)
-        l2 = []
-        for j in idos.all():
-            l2.append(j[0])
         # 读取所有用户信息
         users = Users.query.all()
         users = sorted(users, key=sort_notes, reverse=True)[0:6]
@@ -195,9 +188,28 @@ def friends_views():
         pictures = db.session.query(Note_content).filter(Note_content.type == 2).all()[:4]
         gifs = db.session.query(Note_content).filter(Note_content.type == 3).all()[:4]
         videos = db.session.query(Note_content).filter(Note_content.type == 4).all()[:6]
-        # 判断是否有登录用户(id和name)
+
+        id = session['id']
+        user = Users.query.filter_by(id=id).first()
+        fans = db.session.query(User_attention.user_fan_id).filter(User_attention.user_star_id == id)
+        idos = db.session.query(User_attention.user_star_id).filter(User_attention.user_fan_id == id)
+        l2 = []
+        for j in idos.all():
+            l2.append(j[0])
+
+        note_info = request.args["note_info"]
+        notes = []
+        if note_info == "粉丝":
+            for fan in fans.all():
+                notes.extend(Notes.query.filter_by(user_id=fan.user_fan_id).all())
+                print(notes)
+        if note_info == "关注":
+            for ido in idos.all():
+                notes.extend(Notes.query.filter_by(user_id=ido.user_star_id).all())
+                print(notes)
+        notes = notes[::-1]
         # 每页显示的记录数量
-        pageSize = 20
+        pageSize = 15
         # 接收前段传过来的参数page,,如果没有传递过来，则默认为１，并保存在page变量中
         page = int(request.args.get('page', 1))
         ost = (page - 1) * pageSize
@@ -206,11 +218,10 @@ def friends_views():
             ost = (choosePage - 1) * pageSize
             page = choosePage
         # notes = db.session.query(Notes).offset(ost).limit(pageSize).all()
-        notes = Notes.query.all()[::-1]
-        notes = notes[ost:(ost + pageSize)]
         # 根据pageSize计算尾页
-        totalCount = db.session.query(Notes).count()
+        totalCount = len(notes)
         lastPage = math.ceil(totalCount / pageSize)
+        notes = notes[ost:(ost + pageSize)]
         # 计算上一页
         prePage = page - 1
         if prePage < 1:
@@ -232,19 +243,26 @@ def myspace_views():
         user = Users.query.filter_by(id=id).first()
         fans = db.session.query(User_attention.user_fan_id).filter(User_attention.user_star_id == id)
         idos = db.session.query(User_attention.user_star_id).filter(User_attention.user_fan_id == id)
+
+        l1 = []
+        for i in fans.all():
+            l1.append(Users.query.filter_by(id=i[0]).first())
+
         l2 = []
         for j in idos.all():
-            l2.append(j[0])
-        # 读取所有用户信息
-        users = Users.query.all()
-        users = sorted(users, key=sort_notes, reverse=True)[0:6]
-        words = db.session.query(Note_content).filter(Note_content.type == 1).all()[:6]
-        pictures = db.session.query(Note_content).filter(Note_content.type == 2).all()[:4]
-        gifs = db.session.query(Note_content).filter(Note_content.type == 3).all()[:4]
-        videos = db.session.query(Note_content).filter(Note_content.type == 4).all()[:6]
-        # 判断是否有登录用户(id和name)
+            l2.append(Users.query.filter_by(id=j[0]).first())
+
+        note_info = request.args["note_info"]
+        notes = []
+        if note_info == "my":
+            notes = Notes.query.filter_by(user_id=id).all()
+        if note_info == "addblog":
+            user_note = User_note_attention.query.filter_by(user_id = id).all()
+            for un in user_note:
+                notes.append(Notes.query.filter_by(id=un.note_id).first())
+        notes = notes[::-1]
         # 每页显示的记录数量
-        pageSize = 20
+        pageSize = 15
         # 接收前段传过来的参数page,,如果没有传递过来，则默认为１，并保存在page变量中
         page = int(request.args.get('page', 1))
         ost = (page - 1) * pageSize
@@ -253,11 +271,10 @@ def myspace_views():
             ost = (choosePage - 1) * pageSize
             page = choosePage
         # notes = db.session.query(Notes).offset(ost).limit(pageSize).all()
-        notes = Notes.query.filter_by(user_id=id).all()[::-1]
-        notes = notes[ost:(ost + pageSize)]
         # 根据pageSize计算尾页
-        totalCount = Notes.query.filter_by(user_id=id).count()
+        totalCount = len(notes)
         lastPage = math.ceil(totalCount / pageSize)
+        notes = notes[ost:(ost + pageSize)]
         # 计算上一页
         prePage = page - 1
         if prePage < 1:
@@ -294,7 +311,11 @@ def info_views():
     note = Notes.query.filter_by(id=note_id).first()
     if int(note_id) > 1:
         prev_note_id = int(note_id) - 1
+    else:
+        prev_note_id = 1
     if int(note_id) < Notes.query.count():
         next_note_id = int(note_id) + 1
+    else:
+        next_note_id = Notes.query.count()
 
     return render_template("read.html", params=locals())
